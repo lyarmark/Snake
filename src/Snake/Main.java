415156402fc8bdefba8ed196f799279ded070809
Gen Student1 <student1@student.touro.edu>
2016-02-11 18:45:19 -0500

    moved jframe setUp to jframe class

---
+++ src/Snake/Main.java
@@ -0,0 +1,14 @@
+package Snake;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Creating the window with all its awesome snaky features
+		Window snakeWindow = new Window();
+
+		// Setting up the window settings
+		snakeWindow.setVisible(true);
+
+	}
+}
--- src/Snake/SquarePanel.java
+++ src/Snake/SquarePanel.java
@@ -1,18 +1,18 @@
+package Snake;
 import java.awt.Color;
 import javax.swing.JPanel;
 
-public class SquarePanel extends JPanel{
-	
+public class SquarePanel extends JPanel {
+
 	private static final long serialVersionUID = 1L;
 
-	public SquarePanel(Color d){
+	public SquarePanel(Color d) {
 		this.setBackground(d);
 	}
-	
-	public void ChangeColor(Color d){
+
+	public void ChangeColor(Color d) {
 		this.setBackground(d);
 		this.repaint();
 	}
-	
-}
 
+}
